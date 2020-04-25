46be5c4d8f2b692c11741cb15c9b4280726c714f
alexsiqingliu <uilxela7@gmail.com>
2020-04-25 14:10:15 +0800

    add appbar

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Books from '../books/books';
 
+import PrimarySearchAppBar from '../Appbar';
+
+// console.log(PrimarySearchAppBar);
+
 class MainPage extends React.Component {
   
   render() {
     return (
       <div>
+        <PrimarySearchAppBar />
         <h1>Bookmarker</h1>
         <Books />
       </div>
