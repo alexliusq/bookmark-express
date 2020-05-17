3ad550b8bfc7fefb6a0938a6db1410f505d539e1
alexliusq <uilxela7@gmail.com>
2020-05-17 12:45:47 +0800

    added user_id functionality to tags route, decided testing is going to be too painful until i build out the frontend

--- routes/api/tags.js
+++ routes/api/tags.js
@@ -4,34 +4,36 @@ const router = express.Router();
 const Tags = require('../../models/tags');
 
 const {isEmpty} = require('validator');
+const {getUserIDFromReq} = require('../../validation/users');
 
 
 router.post('/', (req, res) => {
   const annotation_id = req.body.annotation_id + '' || "";
   const tag = req.body.tag || "";
+  const user_id = getUserIDFromReq(req);
   if (isEmpty(annotation_id) || isEmpty(tag)) {
     return res.status(400).json({'error': 'requires both valid annotation Id and tag'})
   }
-  console.log(annotation_id, tag);
-  Tags.addTagToAnnotation(annotation_id, tag)
+
+  Tags.addTagToAnnotation(annotation_id, tag, user_id)
     .then(annoTagIDs => res.json(annoTagIDs))
     .catch(err => res.json(err));
 });
 
 router.get('/', (req, res) => {
-  Tags.getAllTags().then(allTags => res.json(allTags))
+  const user_id = getUserIDFromReq(req);
+  Tags.getAllTags(user_id).then(allTags => res.json(allTags))
     .catch(err => res.json(err));
 });
 
 router.delete('/', (req, res) => {
   const annotation_id = req.body.annotation_id + '' || "";
   const tag = req.body.tag || "";
-  console.log('hello');
-  console.log(annotation_id, tag);
+  const user_id = getUserIDFromReq(req);
   if (isEmpty(annotation_id) || isEmpty(tag)) {
     return res.status(400).json({'error': 'requires both valid annotation Id and tag'})
   }
-  Tags.removeTagFromAnnotation(annotation_id, tag)
+  Tags.removeTagFromAnnotation(annotation_id, tag, user_id)
     .then(annoTagIDs => res.json(annoTagIDs))
     .catch(err => res.json(err));
 })
